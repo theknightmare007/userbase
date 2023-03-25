02030a784577c87966587e4636638a8d4f491cba
0xTheKnightmare <pdiuec@gmail.com>
2023-03-25 21:54:10 +0530

    routing complete

---
+++ src/pages/User/volunteer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const Volunteer = () => {
+    return (
+        <div>
+            <h1 align="center">Want to Volunteer?</h1>
+            <p>
+                As much as we like to 
+            </p>           
+        </div>
+    )
+}
